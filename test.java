9313efdfc5493784158cdae97d748479552c4bc2
Om Jadhav <74755805+Omjadhav13@users.noreply.github.com>
2025-11-11 18:10:43 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,12 +1,34 @@
 package org.yccair.demo_project;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String s="ABONDENED";
+	public static int nameLength(String name) {
+		return name.length();
+	}
+
+	public static String randomWords(int length) {
+		String randomwords=null;
+		List <String> nameList=new ArrayList<String>();
+		nameList.add("ABONDENED");
+		nameList.add("Adventure");
+		nameList.add("Crocodile");
+		nameList.add("Butterfly");
+		nameList.add("Volcanoes");
+		nameList.add("Happiness");
+		nameList.add("Dangerous");
+		nameList.add("Knowledge");
+		nameList.add("Character");
+		nameList.add("Important");
+		randomwords=nameList.get(length);
+
+		return randomwords;
+	}
+	public static void displayMatrix(String s) {
+		
 		
 		String[][] st=new String [3][3];
 		
@@ -26,13 +48,37 @@ public class test {
 			}
 			System.out.println();
 		}
-		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
 		Scanner sc=new Scanner(System.in);
 		String res=sc.next();
-		if(res.equalsIgnoreCase(s)) {
-			System.out.println("won");
-		}
+		String name;
+	
+		int choice=0;
+		do {
+			System.out.println("Word Puzzle");
+			System.out.println("============");
+			System.out.println("Enter your name");
+			name=sc.next();
+			String s= randomWords(nameLength(name));
+			displayMatrix(s);
+			if(res.equalsIgnoreCase(s)) {
+				System.out.println("You won the game!....");
+			}
+			else {
+				System.err.println("You Gussed wrong word");
+				System.err.println("Try again");
+			}
+			
+			
+		}while(choice!= -1);
+		
+		
+		
+		
+		
 	}
 
 }
